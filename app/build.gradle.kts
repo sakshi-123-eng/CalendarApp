b66e2fbb6da68239ba8967191be9fd5a4571d8da
sakshigupta1 <sakshigupta@gofynd.com>
2023-09-03 22:57:42 +0530

    commented code removed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
-    id ("dagger.hilt.android.plugin")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -56,19 +56,8 @@ dependencies {
 
 
     implementation("com.google.dagger:hilt-android:2.45")
-    kapt ("com.google.dagger:hilt-android-compiler:2.45")
+    kapt("com.google.dagger:hilt-android-compiler:2.45")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-//    implementation ("com.google.dagger:hilt-android:2.44")
-////    implementation("com.google.dagger:hilt-compiler:2.44")
-//    kapt ("com.google.dagger:hilt-android-compiler:2.44")
-//
-//    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
-//
-//    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-//    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-//
-//    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-//    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,13 +8,9 @@ buildscript {
 
 
     dependencies {
-//        classpath("com.android.tools.build:gradle:8.1.0")
         classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
         classpath ("com.google.dagger:hilt-android-gradle-plugin:2.44")
         classpath ("com.google.gms:google-services:4.3.15")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
     }
 }
